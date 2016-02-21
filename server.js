a3b6fcce586c5a85965e2b7cdd2dd1bda643d437
BreezeDust <BreezeDust.com@gmail.com>
2016-02-22 01:11:38 +0800

    优化netbase

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mystars = require('./api/mystars');
-//var net=require('./tools/NetBase');
+var net=require('./tools/NetBase');
 //var callBack=net.get("https://www.baidu.com/");
 //callBack.on('end', function(body) {
 //    console.log(body);
@@ -9,6 +9,7 @@ var mystars = require('./api/mystars');
 //callBack.on('error',function(e){
 //    console.log(e);
 //});
+
 app.use('/mystars', mystars);
 
 app.use(express.static('public'));
--- tools/NetBase.js
+++ tools/NetBase.js
@@ -1,9 +1,19 @@
 /**
  * Created by BreezeDust on 16/2/19.
+ * 简单的发送基本请求的类
  */
 var http = require('http');
 var https = require('https');
 var URL = require('url');
+var QS = require('querystring');
+/**
+ * 链接类型
+ * @type {{HTTP: 1, HTTPS: 2}}
+ */
+var LINKTYPE={
+    HTTP:1,
+    HTTPS:2
+};
 
 function CallBack(){
     this.method=[];
@@ -12,50 +22,99 @@ function CallBack(){
     };
     return this;
 }
+/**
+ * 简单get请求
+ * @param url
+ * @param jsonData
+ * @returns {*}
+ */
 function get(url,jsonData){
     var parseUrl=URL.parse(url);
-    //console.log(parseUrl);
+    if(jsonData!=null){
+        // 数据拼接
+        var dataStr=QS.stringify(jsonData);
+        var newQuery="";
+        if(parseUrl.query==""){
+            newQuery=dataStr;
+        }
+        else{
+            newQuery=parseUrl.query+"&"+dataStr;
+        }
+        parseUrl.query=newQuery;
+        parseUrl.path=parseUrl.pathname+parseUrl.query;
+    }
+    console.log(parseUrl);
+    console.log(dataStr);
+    var options = {
+        hostname: parseUrl.hostname,
+        port: parseUrl.port,
+        path: parseUrl.path,
+        method: 'GET',
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    var selectType=checkLinkType(parseUrl);
+    console.log(options);
+    return sendRequest(options,selectType);
+}
+/**
+ * 简单post请求
+ * @param url
+ * @param jsonData
+ * @returns {*}
+ */
+function post(url,jsonData){
+    var parseUrl=URL.parse(url);
+    var dataStr="";
+    if(jsonData!=null){
+        dataStr=QS.stringify(jsonData);
+    }
+    var options = {
+        hostname: parseUrl.hostname,
+        port: parseUrl.port,
+        path: parseUrl.path,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': dataStr.length,
+            'User-Agent': 'request'
+        }
+    };
+    var linkType=checkLinkType(parseUrl);
+    console.log(options);
+    return sendRequest(options,linkType,dataStr);
+}
+/**
+ * 处理选择类型
+ * @param parseUrl
+ * @returns {*}
+ */
+function checkLinkType(parseUrl){
     if(parseUrl.protocol.indexOf("http:")==0){
-        if(parseUrl.port==null){
+        if(parseUrl.port==null || parseUrl.port==""){
             parseUrl.port="80";
         }
-        var options = {
-            hostname: parseUrl.hostname,
-            port: parseUrl.port,
-            path: parseUrl.path,
-            method: 'GET',
-            headers: {
-                'User-Agent': 'request'
-            }
-        };
-        console.log("a");
+        return LINKTYPE.HTTP;
     }
     else if(parseUrl.protocol.indexOf("https:")==0){
-        if(parseUrl.port==null){
+        if(parseUrl.port==null || parseUrl.port==""){
             parseUrl.port="443";
         }
-        var options = {
-            hostname: parseUrl.hostname,
-            port: parseUrl.port,
-            path: parseUrl.path,
-            method: 'GET',
-            headers: {
-                'User-Agent': 'request'
-            }
-        };
-        console.log(options);
-        return sendHttps(options);
+        return LINKTYPE.HTTPS;
     }
 }
-function post(){
-}
-
-function sendHttp(options){
-
-}
-function sendHttps(options){
+/**
+ * 发送request请求
+ * @param options
+ * @param linkType
+ * @returns {CallBack}
+ * @param postData
+ */
+function sendRequest(options,linkType,postData){
     var callBack=new CallBack();
-    var req = https.request(options, function(res){
+    // 请求后回调
+    var reqCallback=function(res){
         var body = '';
         res.on('data', function(chunk) {
             body += chunk;
@@ -65,15 +124,28 @@ function sendHttps(options){
                 callBack.method["end"](body);
             }
         });
-    });
-    req.end();
+    };
+    var req=null;
+    if (linkType == LINKTYPE.HTTP) {
+        req = http.request(options, reqCallback);
+    }
+    else {
+        req = https.request(options, reqCallback);
+    }
     req.on('error',function(e){
         if(callBack.method["error"]!=null){
             callBack.method["error"](e);
         }
     });
+    // 处理post请求的数据
+    if(postData!=null && typeof(postData)=="string"){
+        req.write(postData);
+    }
+    req.end();
     return callBack;
 }
+
+///////////////////
 exports.get=get;
 exports.post=post;
 
