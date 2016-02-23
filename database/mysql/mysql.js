aabed7188de348f3f307daaf521c0a31a8e74696
BreezeDust <BreezeDust.com@gmail.com>
2016-02-23 22:36:38 +0800

    add mysql

---
+++ database/mysql/mysql.js
@@ -0,0 +1,33 @@
+/**
+ * Created by BreezeDust on 16/2/22.
+ */
+var mysql = require('mysql');
+var config = require("/config");
+var pool = mysql.createPool(
+    {
+        connectionLimit : config.mysql.connectionLimit,
+        host: config.mysql.host,
+        user: config.mysql.user,
+        password: config.mysql.password,
+        database: config.mysql.database,
+        port: config.mysql.port
+    }
+);
+function end(){
+    pool.end();
+}
+function query(sql,callback) {
+    pool.getConnection(function (err, connection) {
+        if (!!err) {
+            console.error('[sqlqueryErr] ' + err.stack);
+            connection.release();
+            throw err;
+        }
+        connection.query(sql, function (err, res) {
+            connection.release();
+        });
+    });
+}
+
+exports.query=query;
+
